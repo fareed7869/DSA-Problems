58f5e7e903e639305d995a2ae545d89a6a48c04b
Fareed <fareed.ch7869@gmail.com>
2024-07-31 23:48:50 +0500

    Solve Chunked Arr Problem

--- chunkArray/chunk-Array.js
+++ chunkArray/chunk-Array.js
@@ -1,1 +1,14 @@
+function chunkArray(arr, size) {
+    let chunkedArr = [];
+    for (let i = 0; i < arr.length; i += size) {
+      // Slice the array from index i to i + size
+      chunkedArr.push(arr.slice(i, i + size));
+    }
+    return chunkedArr;
+  }
+ 
+  let array = [1, 2, 3, 4, 5, 6, 7,8,9];
+  let size = 4;
+  let chunkedArray = chunkArray(array, size); // Output: [[1,2,3,4], [5,6,7,8], [9]]
 
+  
