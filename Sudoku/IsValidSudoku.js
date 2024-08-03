1983dcbf8095c94575c54f37c97ccf05d3b4cafb
Fareed <fareed.ch7869@gmail.com>
2024-08-03 23:54:41 +0500

    Solve Is Valid Sudoku Board Problem

--- Sudoku/IsValidSudoku.js
+++ Sudoku/IsValidSudoku.js
@@ -1,1 +1,39 @@
+function isValidSudoku(board) {
 
+    let rows = new Array(9).fill(null).map(() => new Set());
+    let cols = new Array(9).fill(null).map(() => new Set());
+    let boxes = new Array(9).fill(null).map(() => new Set());
+
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+
+        let num = board[i][j];
+        if (num === ".") continue;
+
+        let boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+
+        if (rows[i].has(num) || cols[j].has(num) || boxes[boxIndex].has(num)) {
+          return false;
+        }
+
+        rows[i].add(num);
+        cols[j].add(num);
+        boxes[boxIndex].add(num);
+      }
+    }
+    return true;
+  }
+  
+  const board = [
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+  ];
+  
+  console.log(isValidSudoku(board)); // Output: true
