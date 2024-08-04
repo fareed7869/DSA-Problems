b14290038e6eb9996afae4a4d39caccffdde8c14
Fareed <fareed.ch7869@gmail.com>
2024-08-05 01:29:30 +0500

    Solve Longest Substring Without Repeating Character Problem

--- Longest-Substring-Without-Repeating-Characters/LongestSubstringWithoutRepeatingChar.js
+++ Longest-Substring-Without-Repeating-Characters/LongestSubstringWithoutRepeatingChar.js
@@ -1,1 +1,18 @@
+function longestSubstringWithoutRepeatingCharacters(str) {
+    let windowStart = 0;
+    let maxLength = 0;
+    const charIndexMap = {};
 
+    for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+        const rightChar = str[windowEnd];
+
+        if (rightChar in charIndexMap) {
+            windowStart = Math.max(windowStart, charIndexMap[rightChar] + 1);
+        }
+        charIndexMap[rightChar] = windowEnd;
+        maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+    }
+    return maxLength;
+}
+const str = "abcabcbsbsdr";
+console.log(longestSubstringWithoutRepeatingCharacters(str)); // Output: 3 ("abc")
