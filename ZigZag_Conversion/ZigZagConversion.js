8224cbab6230ef758b2dd166701350a51869dd4c
Fareed <fareed.ch7869@gmail.com>
2024-08-07 01:56:41 +0500

    Solve ZigZag Conversion Problem

--- ZigZag_Conversion/ZigZagConversion.js
+++ ZigZag_Conversion/ZigZagConversion.js
@@ -1,1 +1,23 @@
 
+function ZigZag_Convertion(str, numRows) {
+    if (numRows === 1 || numRows >= str.length) {
+        return str;
+    }
+
+    const rows = new Array(numRows).fill("");
+    let currentRow = 0;
+    let goingDown = false;
+
+    for (let i = 0; i < str.length; i++) {
+        rows[currentRow] += str[i];
+    
+        if (currentRow === 0 || currentRow === numRows - 1) { 
+            goingDown = !goingDown;                            
+        } 
+
+        currentRow += goingDown ? 1 : -1;
+    }
+    return rows.join("");
+}
+
+console.log(ZigZag_Convertion("PAYPALISHIRING", 3)); // Output: "PAHNAPLSIIGYIR"
