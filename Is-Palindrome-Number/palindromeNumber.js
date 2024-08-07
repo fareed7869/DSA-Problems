c8fc599be676f28bdbd1c9186cd0ecc57747a571
Fareed <fareed.ch7869@gmail.com>
2024-08-07 18:21:51 +0500

    Solve Is Palindrome Number Problem

--- Is-Palindrome-Number/palindromeNumber.js
+++ Is-Palindrome-Number/palindromeNumber.js
@@ -1,1 +1,19 @@
+function isPalindrome(num) {
+    // Negative numbers are not palindromes
+    if (num < 0) {
+        return false;
+    }
 
+    let original = num;
+    let reversed = 0;
+
+    while (num > 0) {
+        let digit = num % 10;
+        reversed = reversed * 10 + digit;
+        num = Math.floor(num / 10);
+    }
+    
+    return original === reversed;
+}
+console.log(isPalindrome(121));  // Output: true
+console.log(isPalindrome(-121)); // Output: false
