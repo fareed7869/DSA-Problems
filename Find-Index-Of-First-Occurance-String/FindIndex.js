532f7c701725e14c844732829d1da42bec4b3719
Fareed <fareed.ch7869@gmail.com>
2024-08-18 19:56:53 +0500

    Solve Find Index Of First Occurrence In String Problem

--- Find-Index-Of-First-Occurance-String/FindIndex.js
+++ Find-Index-Of-First-Occurance-String/FindIndex.js
@@ -1,1 +1,17 @@
+function strStr(haystack, needle) {
+    if (needle === "") return 0;
 
+    let needleLength = needle.length;
+    let haystackLength = haystack.length;
+
+    for (let i = 0; i <= haystackLength; i++) {
+        if (haystack.slice(i, i + needleLength) === needle) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// Example usage:
+console.log(strStr("sadbutsad", "sad")); // Output: 0
+console.log(strStr("leetcode", "leeto")); // Output: -1
