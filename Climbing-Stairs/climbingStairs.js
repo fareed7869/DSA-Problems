f91203334f2cfc44c1943d9a2883196136c637b4
Fareed <fareed.ch7869@gmail.com>
2024-08-20 20:42:27 +0500

    Solve Climbing Stair Case Problem

--- Climbing-Stairs/climbingStairs.js
+++ Climbing-Stairs/climbingStairs.js
@@ -1,1 +1,13 @@
+function climbingStairCase(n){
 
+    let numOfWays=[1,2];
+
+    for(let i=2;i<=n;i++){
+        console.log("check",numOfWays)
+        numOfWays[i]=numOfWays[i-1]+numOfWays[i-2]
+    }
+
+    return numOfWays[n-1];
+}
+
+console.log(climbingStairCase(5)) // result 8
