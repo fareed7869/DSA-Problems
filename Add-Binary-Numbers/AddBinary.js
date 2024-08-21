5d77fb8f024e9e7d8b74d16a628740a410524182
Fareed <fareed.ch7869@gmail.com>
2024-08-21 19:39:06 +0500

    Solve Add Binary Numbers Problem

--- Add-Binary-Numbers/AddBinary.js
+++ Add-Binary-Numbers/AddBinary.js
@@ -1,1 +1,29 @@
+function addBinary(a, b) {
 
+    let i = a.length - 1;
+    let j = b.length - 1;
+    let carry = 0;
+    let result = [];
+
+    while (i >= 0 || j >= 0 || carry > 0) {
+        let sum = carry;
+
+        if (i >= 0) {
+            sum += parseInt(a[i]);
+            i--;
+        }
+
+        if (j >= 0) {
+            sum += parseInt(b[j]);
+            j--;
+        }
+
+        result.unshift(sum % 2);
+        carry = Math.floor(sum / 2);
+    }
+
+    return result.join('');
+}
+
+console.log(addBinary("11", "1")); // Output: "100"
+console.log(addBinary("1010", "1011")); // Output: "10101"
