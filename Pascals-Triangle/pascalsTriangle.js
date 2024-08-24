509d3b007921db600acb8555cb3bf5f9fb6b5124
Fareed <fareed.ch7869@gmail.com>
2024-08-24 21:52:57 +0500

    Solve Pascals Triangle Problem

--- Pascals-Triangle/pascalsTriangle.js
+++ Pascals-Triangle/pascalsTriangle.js
@@ -1,1 +1,33 @@
+function pascalsTriangle(numRows) {
 
+    const triangle = [];
+
+    for (let i = 0; i < numRows; i++) {
+
+        const row = [1];
+
+        for (let j = 1; j < i; j++) {
+            row.push(triangle[i - 1][j - 1] + triangle[i - 1][j]);
+        }
+
+        if (i > 0) {
+            row.push(1);
+        }
+
+        triangle.push(row);
+    }
+    
+    return triangle;
+}
+
+console.log(pascalsTriangle(5));
+/* Output:
+[
+    [1],
+    [1, 1],
+    [1, 2, 1],
+    [1, 3, 3, 1],
+    [1, 4, 6, 4, 1]
+]
+*/
+console.log(pascalsTriangle(1)); // Output: [[1]]
