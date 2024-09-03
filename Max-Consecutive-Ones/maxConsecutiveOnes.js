37cafd6e82136b3f8be65e0a6bf173c7fbb2d5c9
Fareed <fareed.ch7869@gmail.com>
2024-09-03 22:42:48 +0500

    Solve Find Max Consecutive Ones in Arr

--- Max-Consecutive-Ones/maxConsecutiveOnes.js
+++ Max-Consecutive-Ones/maxConsecutiveOnes.js
@@ -1,1 +1,21 @@
+function maxConsecutiveOnes(arr) {
+    let maxCount = 0;
+    let count = 0;
+    let num = 1;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === num) {
+            count++;
+        } else {
+            maxCount = Math.max(maxCount, count);
+            count = 0;
+        }
+    }
+
+    maxCount = Math.max(maxCount, count);
+    return maxCount;
+}
+
+console.log(maxConsecutiveOnes([1, 1, 0, 1, 1, 1])); // Output: 3
+console.log(maxConsecutiveOnes([1, 0, 1, 1, 0, 1])); // Output: 2
 
