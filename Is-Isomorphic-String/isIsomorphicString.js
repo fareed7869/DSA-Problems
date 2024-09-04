b87450fa884748f8ef2991867cd8599e397492a6
Fareed <fareed.ch7869@gmail.com>
2024-09-04 20:32:23 +0500

    Solve Is Isomorphic Strings Problem

--- Is-Isomorphic-String/isIsomorphicString.js
+++ Is-Isomorphic-String/isIsomorphicString.js
@@ -1,1 +1,31 @@
+function isIsomorphic(s, t) {
+    if (s.length !== t.length) return false;
 
+    const mapST = {};
+    const mapTS = {};
+
+    for (let i = 0; i < s.length; i++) {
+        const charS = s[i];
+        const charT = t[i];
+
+        // Check if there's already a mapping for charS in mapST
+        if (mapST[charS] === undefined) { // undefined means not exist
+            mapST[charS] = charT;
+        } else if (mapST[charS] !== charT) {
+            return false;
+        }
+
+        // Check if there's already a mapping for charT in mapTS
+        if (mapTS[charT] === undefined) {
+            mapTS[charT] = charS;
+        } else if (mapTS[charT] !== charS) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(isIsomorphic("egg", "add"));   // Output: true
+console.log(isIsomorphic("foo", "bar"));   // Output: false
+console.log(isIsomorphic("paper", "title")); // Output: true
