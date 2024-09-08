c4646e49cdd8d978bd3e55219858958b2f35208a
Fareed <fareed.ch7869@gmail.com>
2024-09-08 20:38:10 +0500

    Solve Is Happy Number Problem

--- Happy-Number/isHappyNumber.js
+++ Happy-Number/isHappyNumber.js
@@ -1,1 +1,27 @@
+function isHappy(n) {
+    const seen = new Set();  // To detect cycles, if number repeat
+    
+    function sumOfSquares(num) {
+        let sum = 0;
+        while (num > 0) {
+            let digit = num % 10;
+            sum += digit * digit;
+            num = Math.floor(num / 10);
+        }
+        return sum;
+    }
 
+    while (n !== 1) {
+        n = sumOfSquares(n);
+        // If n is already in the set, a cycle is detected
+        if (seen.has(n)) {
+            return false;
+        }
+        seen.add(n);
+    }
+    // If n becomes 1 then it is happy number
+    
+    return true;
+}
+console.log(isHappy(19)); // Output: true
+console.log(isHappy(2));  // Output: false
